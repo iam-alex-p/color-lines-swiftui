ccd3cbe3dc08e106be03feabe92750dfc807a124
Alex P <alexey.pokolev@gmail.com>
2023-01-09 17:45:05 -0800

    Initial Commit

---
+++ ColorLines/ColorLinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorLinesApp.swift
+//  ColorLines
+//
+//  Created by Aleksei Pokolev on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorLinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
